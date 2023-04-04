0615c2f5b5ea3ce5fb00618084805ac4eadc7561
Drew <drc920630@gmail.com>
2023-04-05 00:25:33 +0900

    서버사이드렌더링 제대로 학습 getServerSideProps를 잊지 말자

--- next.config.js
+++ next.config.js
@@ -1,8 +1,34 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = "ebb5950759cebf73ef7710cb9b73f4c4";
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  //리액트 스트릭트모드
+  // reactStrictMode: false,
+  //API키 숨기는 설정(.env로도 가능)
+  async redirects(){
+    return [
+      {
+        //redirect(특정 주소로 오면 다른 주소로 보내는 것 /old-blog/:path 이렇게 파람스도 가능)
+        //소스에 뭐라고 치면
+        source: "/contact",
+        //이곳으로 보낸다. (*를 뒤에 붙이면 아무렇게나 쳐도 destination으로 이동한다.)
+        destination: "/form",
+        permanent: false,
+      }
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/moives",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development"
+import { useState, useEffect } from "react"
 import Seo from "../components/Seo";
 
 //Next.js는 React.js를 백엔드에서 동작시켜서 페이지를 미리 랜더링함
@@ -7,13 +7,32 @@ import Seo from "../components/Seo";
 //그리고 JS작동을 멈추면, Next가 그려준 HTML의 UI는 유저에게 보이나,
 //아래와 같이 React.js의 기능인 useState가 작동하지 않아 버튼이 활성화 되지 않음.
 //이것이 SSR(Server-Side-Rendering)
-export default function Home() {
-  const [counter, setCounter] = useState(0);
+export default function Home({results}) {
   return (
     <div>
       <Seo title={"Home"} />
-      <h1>Hello {counter}</h1>
-      <button onClick={()=> setCounter((cur)=> cur + 1)}>+</button>
+      {/* 데이터가 존재하지 않을 때 로딩 중 이라는 표시 나타내기 */}
+      {!results && <h4>Loading...</h4>}
+      {
+        results?.map(el =>
+        <div key={el.id}>
+          <h4>{el.original_title}</h4>
+        </div>)
+      }
     </div>
   )
+}
+
+//오직 백엔드에서만 작동하는 영역
+//함수명을 뺴먹지 않고 똑같이 적어줘야한다. getServerSideProps
+//객체형태로 리턴해주고, 이것을 위에 Home이 props로 받는다.
+//그 후에 구조분해할당을 하던, props.을 이용하던 프론트 단에서 데이터를 사용하면 된다.
+export async function getServerSideProps() {
+  const API_KEY = "ebb5950759cebf73ef7710cb9b73f4c4";
+  const { results } = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();
+  return {
+    props: {
+      results,
+    }
+  }
 }
